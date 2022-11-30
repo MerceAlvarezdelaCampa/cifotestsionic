cb626869ae59af99788f14534f51d1eebb5a0620
jrbistuer <jrbistuer@gmail.com>
2022-11-30 18:45:13 +0100

    android barscanner

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { BarcodeScanner } from '@capacitor-community/barcode-scanner';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +9,28 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  result: any;
+
+  constructor(public platform: Platform) {}
+
+  async startScan() {
+    console.log(this.platform.is('ios'), this.platform.is('android'));
+    if (this.platform.is('ios') || this.platform.is('android')) {
+      // Check camera permission
+      // This is just a simple example, check out the better checks below
+      await BarcodeScanner.checkPermission({ force: true });
+    
+      // make background of WebView transparent
+      // note: if you are using ionic this might not be enough, check below
+      BarcodeScanner.hideBackground();
+    
+      const result = await BarcodeScanner.startScan(); // start scanning and wait for a result
+    
+      // if the result has content
+      if (result.hasContent) {
+        this.result = result.content; // log the raw scanned content
+      }
+    }
+  };
 
 }
